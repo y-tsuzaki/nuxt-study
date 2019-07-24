b8e2929b4c56ac23c95f2204d47dd24559ced77b
y-tsuzaki <y-tsuzaki@macloud.jp>
2019-07-24 18:34:10 +0900

    contentfulとつなぎこみ

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,13 @@
+const pkg = require('./package');
+const {getConfigForKeys} = require('./lib/config.js');
+const ctfConfig = getConfigForKeys([
+  'CTF_BLOG_POST_TYPE_ID',
+  'CTF_SPACE_ID',
+  'CTF_CDA_ACCESS_TOKEN',
+]);
+
+const {createClient} = require('./plugins/contentful');
+const cdaClient = createClient(ctfConfig);
 
 export default {
   mode: 'universal',
@@ -7,33 +17,34 @@ export default {
   head: {
     title: process.env.npm_package_name || '',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || '',
+      },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+    ],
   },
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: {color: '#fff'},
   /*
   ** Global CSS
   */
-  css: [
-  ],
+  css: [],
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [
-  ],
+  plugins: [],
   /*
   ** Nuxt.js modules
   */
-  modules: [
-  ],
+  modules: [],
   /*
   ** Build configuration
   */
@@ -42,6 +53,19 @@ export default {
     ** You can extend webpack config here
     */
     extend(config, ctx) {
-    }
-  }
-}
+    },
+  },
+  generate: {
+    routes() {
+      return cdaClient.getEntries(ctfConfig.CTF_BLOG_POST_TYPE_ID).
+        then(entries => {
+          return [...entries.items.map(entry => `/blog/${entry.fields.slug}`)];
+        });
+    },
+  },
+  env: {
+    CTF_SPACE_ID: ctfConfig.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: ctfConfig.CTF_CDA_ACCESS_TOKEN,
+    CTF_BLOG_POST_TYPE_ID: ctfConfig.CTF_BLOG_POST_TYPE_ID,
+  },
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,71 +1,34 @@
 <template>
-  <div class="container">
-    <div>
-      <div class="inline" v-for="n in kanpaiCount">
-        <logo />
-      </div>
-      <div class="links">
-        <button class="kanpaiButton" @click="kanpai">乾杯する！</button>
-      </div>
-    </div>
-  </div>
+  <section class="index">
+    <card
+      v-for="(post,i ) in posts"
+      :key="i"
+      :title="post.fields.title"
+      :id="post.sys.id"
+      :date="post.sys.updatedAt"
+    />
+  </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+  import Card from '~/components/card.vue'
+  import { createClient } from '~/plugins/contentful.js'
 
-export default {
-  components: {
-    Logo
-  },
-
-  data() {
-    return {
-      kanpaiCount : 1,
-    }
-  },
-
-  methods: {
-    kanpai: function () {
-      console.log('kanpai');
-      this.kanpaiCount++;
+  const client = createClient()
+  export default {
+    transition: 'slide-left',
+    components: {
+      Card
+    },
+    asyncData({ env, params }) {
+      return client
+      .getEntries(env.CTF_BLOG_POST_TYPE_ID)
+      .then(entries => {
+        return {
+          posts: entries.items
+        }
+      })
+      .catch(console.error)
     }
   }
-}
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.inline {
-  display: inline-block;
-}
-.kanpaiButton {
-  font-size : 40px;
-}
-</style>
